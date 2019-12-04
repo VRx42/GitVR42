8a09b7f2e187f4dd889f762c2600a47bb0ebd8ba
Victor Ronchin <vic.ronchin@yahoo.fr>
2019-12-04 09:42:29 +0100

    pour l instant 1 ligne sur 2 a peu pres

--- get_next_line.c
+++ get_next_line.c
@@ -14,46 +14,53 @@
 #include <stdio.h>
 
 
-int	make_line(char **line,  char *str)
+int	make_line(char **line,  char **str, char *ptr_str)
 {
 	size_t		i;
 	char		*tmp;
 
-	printf("2");
-	if (ft_strchr(str, '\n') == NULL)//si on a pas de \n alors retourne 0 et apres read un autre buffer dans gnl
-		return (0);// on retourne 0 donc dans le gnl on refait une boucle en gardant dans le static str qvec le debut sans /n
-	else//si on trouve un \n alors go modifier la line et retourner 1 en joingant le str
+	//printf("2")
+	i = 0;
+	while (ptr_str[i] != '\n' && ptr_str[i])
+		i++;
+	if (ptr_str[i] == '\n')
 	{
-		i = 0;
-		while (str[i] != '\n')
-			i++;
-		tmp = str;// str et tmp pointent vers les memes adresses donc on peut free tmp
-		*line = ft_substr(tmp, 0, i + 1);//si on a un  /n alors on met dans le line str jsqu a i. OK pour line, modifiee a distance et malloke
-		str = ft_substr(tmp, i + 1, ft_strlen(str) - (i + 1) + 1); //ici on veut un str qui commence apres i + 1 mais on doit copier sur tmp avant
-		//free(tmp);
-		return (1);
+		(*line) = ft_substr(*str, 0, i);
+		tmp = ft_strdup(&(*str)[i + 1]);
+		free(*str);
+		*str = tmp;
 	}
-	return (0);
+	else
+	{
+		(*line) = ft_strdup(*str);
+		//
+	}	
+	return (1);
 }
 
 int	get_next_line(int fd, char **line)
 {
-	char		buffer[BUFFER_SIZE + 2];
+	char		buffer[BUFFER_SIZE + 1];
 	static char	*str;
+	int 		ret;
+	char		*tmp;
 
-	str = NULL;
-	printf("0.8");
-	if (fd < 0 || line == NULL || BUFFER_SIZE <= 0)
+	if (fd < 0 || line == NULL || BUFFER_SIZE < 0)
 		return (-1);
-	printf("0.9");
-	while (read(fd, buffer, BUFFER_SIZE) > 0)//tant que read > 0 (i.e qu' on a lu quelque chose)
+	if (!str)
 	{
-		buffer[BUFFER_SIZE + 1] = '\0';
+		if (!(str = (char*)malloc(sizeof(char) * (BUFFER_SIZE + 1))))
+			return (-1);
+	}
+
+	while ((ret = read(fd, buffer, BUFFER_SIZE)) > 0)//tant que read > 0 (i.e qu' on a lu quelque chose)
+	{
+		buffer[ret] = '\0';
 		if (buffer[0] == '\0')
 			return (1);
-		printf("1");
-		str = ft_strjoin(str, buffer);//le strjoin free aussi le str entrant donc pas besoin de tmp
-		if (make_line(line, str) == 1)//si pas un alors pas de /n et on refait une lecture pour avoir un /n
+		tmp = ft_strjoin(str, buffer);
+		str = tmp;
+		if (make_line(line, &str, str) == 1)//deux cqs; on a un \n ou pas
 			return (1);
 	}
 	return (0);
@@ -70,7 +77,7 @@ int			main(int ac, char **av)
 	ac = ac + 0;
 	fd = open(av[1], O_RDONLY);
 	//printf("fd = %d\n", fd);
-	while (get_next_line(fd, &line) && i < 10)
+	while ((get_next_line(fd, &line) == 1))
 	{
 		ret = get_next_line(fd, &line);
 		printf("[%d] [%d] |%s|\n", i++, ret,  line);
@@ -79,6 +86,23 @@ int			main(int ac, char **av)
 	close(fd);
 	return (0);
 }
+
+/*
+int   main(int ac, char **av)
+{
+	char  *line;
+	int   fd1;
+	int   fd2;
+
+	fd1 = open(av[1], O_RDONLY);
+	fd2 = open(av[2], O_RDONLY);
+	get_next_line(fd1, &line);
+	printf("%s\n", line);
+	get_next_line(fd1, &line);
+	printf("%s\n", line);
+}*/
+
+
 
 
 /*
