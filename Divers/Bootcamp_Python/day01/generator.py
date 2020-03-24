4cc1dafe7b63bcdb9cd54106bbd3a98496691399
Victor Ronchin <vic.ronchin@yahoo.fr>
2020-03-24 18:27:57 +0100

    travail sur shuffle

--- Divers/Bootcamp_Python/day01/generator.py
+++ Divers/Bootcamp_Python/day01/generator.py
@@ -1,1 +1,41 @@
 
+
+import random
+
+
+def generator(text, sep=" ", option=None):
+    if (isinstance(text, str) == False):
+        exit("Your text is not a text. A text works with letters. Dummy.")
+    if (option == None):
+        for word in text.split(sep):
+            yield (word)
+    elif (option == "shuffle"):
+        reversed = random.shuffle(text.split(sep))
+        print(reversed)
+        for word in random.shuffle(text.split(sep)):
+            yield (word)
+    # elif (option == "ordered"):
+    #     for
+
+
+
+def shuffle_list(lst):
+
+    for i in range(len(lst))
+        
+
+text = "Le Lorem Ipsum est simplement du faux texte."
+i = 0
+for word in generator(text, sep= " ", option="shuffle"):
+    print(word)
+    i += 1
+
+
+lst = ['le', 'Lorem', 'Nopsum', 'Emma']
+
+
+# for word in generator(text, sep=" ", option="shuffle"):
+#     print(word)
+
+# for word in generator(text, sep=" ", option="ordered"):
+#     print(word)
